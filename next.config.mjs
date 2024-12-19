effb0d3aeb27a3afc35b8fb8e37dbdae9a57b2c0
jayan110105 <jayan.mitmpl2022@learner.manipal.edu>
2024-12-19 11:24:19 +0530

    rolled back next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Enable React's strict mode for catching potential issues
-    experimental: {
-      logging: true, // Enable additional logging for debugging
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        // Enables verbose logs on the server-side
-        console.log("Building for the server...");
-      } else {
-        console.log("Building for the client...");
-      }
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
