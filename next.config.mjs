21d4455ea965fb3d08b1da6778a3e2bf94172b33
jayan110105 <jayan.mitmpl2022@learner.manipal.edu>
2024-12-19 11:19:49 +0530

    added logging to next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enable React's strict mode for catching potential issues
+    experimental: {
+      logging: true, // Enable additional logging for debugging
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        // Enables verbose logs on the server-side
+        console.log("Building for the server...");
+      } else {
+        console.log("Building for the client...");
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
